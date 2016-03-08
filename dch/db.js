b3a5e5f9216e3ee4ae5e21620b4f855b5ccfcf2e
Ke Er (Brenda) Xiong <krx.sky@gmail.com>
2016-03-08 06:45:15 -0800

    fix style with mongoose

--- dch/db.js
+++ dch/db.js
@@ -1,24 +1,23 @@
-var MONGOOSE = require('mongoose'); //include mongose moduel 
-var SCHEMA = MONGOOSE.Schema;
+var mongoose = require('mongoose'); //include mongose module
 // TODO: set up a path for the test db
-var MONGODB_URL = 'mongodb://localhost:27017/acc_data';
+var MONGODB_URL = 'mongodb://localhost:27017/mqtt-data';
 
-MONGOOSE.connect(MONGODB_URL); 
+mongoose.connect(MONGODB_URL); 
 //Sucessfully connected to the databse
-MONGOOSE.connection.on('connected', function() {
+mongoose.connection.on('connected', function() {
   console.log('Connected to database');
 });
 
 //Failed the connection to the database
-MONGOOSE.connection.on('err', function(err) {
+mongoose.connection.on('err', function(err) {
   console.log('Unable to connect to the databse '+ err);
 });
 
-MONGOOSE.connection.on('disconnect', function(err) {
+mongoose.connection.on('disconnect', function(err) {
   console.log('The database has been disconnected');
 });
 
-var dataSchema = new SCHEMA({
+var dataSchema = mongoose.Schema({
   watch_id: String, 
   acc_x: Number, 
   acc_y: Number, 
@@ -27,25 +26,25 @@ var dataSchema = new SCHEMA({
 });
 
 //create a model for the accelration data
-var Data = MONGOOSE.model('Data', dataSchema);
+var Data = mongoose.model('Data', dataSchema);
 module.exports.model = Data;
 
 module.exports.disconnect = function(){
-  MONGOOSE.disconnect();
+  mongoose.disconnect();
 };
 
 // callback takes in err, result as params
 module.exports.getData = function(watchID, startTime, stopTime, freq, callback){
   Data.aggregate(
-  { $match: { watch_id: watchID, timestamp: { $gt: startTime, $lt: stopTime}}},
-  { $group: { 
-    _id: { $substract: ['$timestamp', { $mod: ['$timestamp', freq]}]},
-      acc_x: {$avg: '$acc_x'},
-      acc_y: {$avg: '$acc_y'},
-      acc_z: {$avg: '$acc_z'},
-    }},
-  { $project: { acc_z: 1, acc_y: 1, acc_z: 1, timestamp: '$_id'} },
-  callback);
+    { $match: { watch_id: watchID, timestamp: { $gt: startTime, $lt: stopTime}}},
+    { $group: { 
+      _id: { $substract: ['$timestamp', { $mod: ['$timestamp', freq]}]},
+        acc_x: {$avg: '$acc_x'},
+        acc_y: {$avg: '$acc_y'},
+        acc_z: {$avg: '$acc_z'},
+      }},
+    { $project: { acc_z: 1, acc_y: 1, acc_z: 1, timestamp: '$_id'} },
+    callback);
 };
 
 // The callback takes in an error parameter
